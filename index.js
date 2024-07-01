6302a31b2ccb17f23c44faaef20fce7a46fdac33
doelsumbing87 <162757841+doelsumbing87@users.noreply.github.com>
2024-07-01 21:15:25 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+function tambah(angka1, angka2) {
+  return angka1 + angka2;
+}
+
+const angka1 = 5;
+const angka2 = 3;
+const hasil = tambah(angka1, angka2);
+console.log(`Hasil penjumlahan ${angka1} + ${angka2} adalah: ${hasil}`);
